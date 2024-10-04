7dd835503b1822de61769df98dbe8a934c7ed43c
unknown <rosa.angel.daniel@gmail.com>
2024-10-04 17:10:03 -0600

    Added inital api routes

---
+++ src/routes/api/index.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+// need to import routes for users, thoughts, and reactions
+
+const router = Router();
+// need to create router for users, thoughts, and reactions
+
+
+export default router;
